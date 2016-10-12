1b7ff85eebd2e74c49a55bf1ff1e777c39c688b4
Oleh Kuchuk <oleh.kuchuk@globallogic.com>
2016-10-12 18:32:39 +0300

    added isNumber implementation

---
+++ isNumber.js
@@ -0,0 +1,38 @@
+/**
+ * Check if selected value is a number
+ * Simple approach with builtin
+ */
+function isNumber(value) {
+  return NaN.isNaN(Number(value)) ? false : true
+}
+
+/**
+ * Better approach without builtins
+ * Should be optimized
+ */
+function isNumberH(value) {
+  var chars = '.0123456789'
+  var dotHappened = false
+  var status = true
+
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] === '.') {
+      if (!dotHappened) {
+        dotHappened = true
+        continue
+      } else {
+        status = false
+        break
+      }
+    }
+
+    if (chars.indexOf(value[i]) < 0) {
+      status = false
+      break
+    }
+  }
+
+  return status
+}
+
+console.log(isNumberH('22.2'))
